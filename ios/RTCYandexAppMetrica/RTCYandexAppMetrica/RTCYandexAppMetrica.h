c7051043195ec96549733990a286b54d65c35cb8
borapop <n.skryt@leningrad.io>
2018-07-12 11:18:07 +0300

    ios initial

---
+++ ios/RTCYandexAppMetrica/RTCYandexAppMetrica/RTCYandexAppMetrica.h
@@ -0,0 +1,13 @@
+//
+//  RTCYandexAppMetrica.h
+//  RTCYandexAppMetrica
+//
+//  Created by macbook on 12.07.2018.
+//  Copyright © 2018 borapop. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RTCYandexAppMetrica : NSObject
+
+@end
---
+++ ios/RTCYandexAppMetrica/RTCYandexAppMetrica/RTCYandexAppMetrica.m
@@ -0,0 +1,28 @@
+//
+//  RTCYandexAppMetrica.m
+//  RTCYandexAppMetrica
+//
+//  Created by macbook on 12.07.2018.
+//  Copyright © 2018 borapop. All rights reserved.
+//
+
+#import "RTCYandexAppMetrica.h"
+
+#import <React/RCTConvert.h>
+#import <YandexMobileMetrica/YandexMobileMetrica.h>
+
+@implementation RTCYandexAppMetrica {
+    
+}
+
+BOOL dryRun = false;
+BOOL initialized = false;
+
+RCT_EXPORT_MODULE();
+RCT_EXPORT_METHOD(initialize:(NSString *)apiKey)
+{
+    YMMYandexMetricaConfiguration *configuration = [[YMMYandexMetricaConfiguration alloc] initWithApiKey:apiKey];
+    [YMMYandexMetrica activateWithConfiguration:configuration];
+}
+
+@end
